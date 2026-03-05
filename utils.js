5d4a5d98af59443a32f81ac59d919d97118a396f
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 21:28:17 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:27:06.421Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T18:28:17.154Z
+Commit: Add export functionality for reports
