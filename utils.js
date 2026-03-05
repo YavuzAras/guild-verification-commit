e87d35774c7e182f16b2eeaea080733f44e8c220
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 21:26:46 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:26:41.500Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T18:26:46.420Z
+Commit: Add export functionality for reports
