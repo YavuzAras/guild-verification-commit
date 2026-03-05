0d0e845502806a2b0e0027ddbe9b21a2601b15fe
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 21:46:40 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:30:09.544Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T18:46:40.783Z
+Commit: Fix CSS styling bug on mobile devices
