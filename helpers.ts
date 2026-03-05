b3ed0fe1d2aeac92242eba49bf43bfb143ac6624
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 23:27:22 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:20:12.555Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T20:27:22.345Z
+Commit: Patch API endpoint error handling
