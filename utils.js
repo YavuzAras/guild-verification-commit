ad942312054bce3b05953d8992541dbcbba9b406
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 23:20:11 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:14:51.255Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T20:20:11.468Z
+Commit: Fix broken links in documentation
