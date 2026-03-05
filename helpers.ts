bdd83a64a87c2c6c9c9e9afb7a3973b541141fd7
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 21:30:08 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:30:00.701Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T18:30:08.008Z
+Commit: Resolve null pointer exception in user service
