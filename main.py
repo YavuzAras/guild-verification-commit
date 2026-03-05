8aaf56e206a17660f5f6a29ca946829a545c7e4e
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 21:28:12 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:26:49.735Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-05T18:28:12.774Z
+Commit: Fix CSS styling bug on mobile devices
