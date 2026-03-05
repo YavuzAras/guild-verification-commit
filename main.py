b1be0bf120594f1fe4b83d0eb32b16e0f72785dc
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 21:25:32 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:24:49.938Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T18:25:31.947Z
+Commit: Patch API endpoint error handling
