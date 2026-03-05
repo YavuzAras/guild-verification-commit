9b551b048777434d0af238130f17bf2cd7b2e83a
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 21:25:37 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:25:34.749Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T18:25:36.947Z
+Commit: Patch API endpoint error handling
