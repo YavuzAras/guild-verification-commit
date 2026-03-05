782dfc26a597bfd3d146ecdc3df0dc243c39f92c
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 22:11:58 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:46:40.783Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T19:11:57.858Z
+Commit: Add export functionality for reports
