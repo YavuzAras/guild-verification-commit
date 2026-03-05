9bbe1d693d6d71f44e72c30ef5a86e913a79ce3d
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-05 21:27:06 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:26:46.420Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:27:06.421Z
+Commit: Patch security vulnerability in authentication
