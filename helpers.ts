fc2ad7cdfe740a25e372ad89c56f3d5560bc73d5
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-06 13:52:54 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:27:26.320Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T10:52:52.833Z
+Commit: Fix race condition in async operations
