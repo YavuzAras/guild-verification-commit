c815394747a0535eda7c4dd28e5b9884e9aab537
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-07 11:10:52 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T08:10:43.690Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-07T08:10:51.924Z
+Commit: Fix CSS styling bug on mobile devices
