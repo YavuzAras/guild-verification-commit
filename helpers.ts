7b417212e170e7d8d5476ea888b6d0de59fa06db
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-07 11:11:16 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T08:11:11.611Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-07T08:11:15.707Z
+Commit: Patch security vulnerability in authentication
