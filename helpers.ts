22c62825fe73b9d106da9c373b87a2f37f840ed7
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:32:04 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:30:08.347Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T11:32:03.945Z
+Commit: Patch API endpoint error handling
