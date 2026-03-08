2c58d8f17f22d6d71663d0671f26c2d759ea5af0
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:36:52 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:36:46.066Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T11:36:51.922Z
+Commit: Resolve null pointer exception in user service
