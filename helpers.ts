dbd1942a5556209b55a9509e32ea2c6a05271638
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:33:55 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:33:43.028Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-08T11:33:54.748Z
+Commit: Patch API endpoint error handling
