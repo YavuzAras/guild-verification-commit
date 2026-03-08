c9afeb168e9906ce4eacc8eb5ea4c0ecd2e59870
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:33:13 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:33:11.108Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-08T11:33:13.125Z
+Commit: Resolve null pointer exception in user service
