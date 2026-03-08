b5a6e0b3e8dec08dd3b920ab396fbed86f84cc25
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:35:33 +0300

    Introduce real-time notifications

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:35:14.040Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-08T11:35:32.701Z
+Commit: Introduce real-time notifications
