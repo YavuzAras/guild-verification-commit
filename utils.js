9eed326ff4eb107796b78bd2e05700e07e7ad9f0
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:32:14 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:32:04.868Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-08T11:32:13.327Z
+Commit: Patch API endpoint error handling
