1652ee42f4aefc4094dc1ce11e00239be3aa3f53
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:30:13 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:30:04.545Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-08T11:30:12.210Z
+Commit: Fix memory leak in event handlers
