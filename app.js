8d7bbd3896691906aef4cd7d283135cdb2c9e8dc
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:35:14 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:35:04.598Z
-Commit: Add dark mode support
+Timestamp: 2026-03-08T11:35:14.040Z
+Commit: Resolve null pointer exception in user service
