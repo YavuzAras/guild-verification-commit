a689ae5797596fcb2008748f79be0f15312b27b5
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:37:06 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:36:57.714Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T11:37:05.678Z
+Commit: Fix memory leak in event handlers
