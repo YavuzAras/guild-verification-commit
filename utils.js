03b2cfb01fa149bb8bed874ab0828bfe017e2224
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:36:11 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:36:07.186Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-08T11:36:10.823Z
+Commit: Fix memory leak in event handlers
