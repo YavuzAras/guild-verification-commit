617fa54944fc619d17c6efed15fb02b7fc7784f5
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:33:43 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:33:28.515Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-08T11:33:43.028Z
+Commit: Resolve timezone issues in date handling
