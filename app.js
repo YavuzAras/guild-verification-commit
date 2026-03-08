55d47ff081a0546780e7cfba6415800d41c21838
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:30:47 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:30:45.112Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-08T11:30:46.271Z
+Commit: Fix broken links in documentation
