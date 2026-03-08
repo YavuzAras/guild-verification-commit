acbf4a2c358a590bce0a8d834faa03d16155243b
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:36:31 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:36:19.029Z
-Commit: Add multi-language support
+Timestamp: 2026-03-08T11:36:30.813Z
+Commit: Resolve timezone issues in date handling
