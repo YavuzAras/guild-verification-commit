bb130d2ae9aee1452152ab2995ade1ad7039a706
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:36:41 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:36:36.203Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-08T11:36:40.764Z
+Commit: Fix CSS styling bug on mobile devices
