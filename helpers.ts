807564544d8108221ac63337756050ea841d8c77
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:32:48 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:32:11.040Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-08T11:32:47.610Z
+Commit: Fix broken links in documentation
