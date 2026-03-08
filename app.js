a3c5fe64bdf22d563dfe16cddb96e71d659ae855
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-08 14:32:58 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:32:23.401Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-08T11:32:57.727Z
+Commit: Fix broken links in documentation
