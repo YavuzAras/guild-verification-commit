8317ca22e0313f7f93fd1297ab97f00bb25122f2
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-09 09:22:44 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T06:22:36.664Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-09T06:22:43.318Z
+Commit: Correct typo in configuration file
