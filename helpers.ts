ddb526312b743841a2becbfcf859ec62b7b07b65
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-09 09:22:01 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T06:21:49.711Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-09T06:22:00.423Z
+Commit: Correct typo in configuration file
