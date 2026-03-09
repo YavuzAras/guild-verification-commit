4631bc45e288e199c8b155fafacd5759ff957df8
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-09 09:06:20 +0300

    Fix race condition in async operations

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T11:36:58.853Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-09T06:06:19.682Z
+Commit: Fix race condition in async operations
