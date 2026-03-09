732847382354b83ef8895fe81033a86aedda2e18
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-09 09:30:10 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T06:29:57.808Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-09T06:30:10.071Z
+Commit: Fix race condition in async operations
