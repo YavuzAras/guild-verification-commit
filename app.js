3b0e8248d1254ef33cdf5ad46355505b146f77b2
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-11 14:14:17 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:14:13.811Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-11T11:14:16.923Z
+Commit: Fix broken links in documentation
