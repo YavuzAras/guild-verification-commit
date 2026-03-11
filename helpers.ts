a0b6b0e638353f6ef2f80187660b6f0c34a993ca
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-11 14:13:11 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:12:44.270Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-11T11:13:11.043Z
+Commit: Implement user profile customization
