d22d7b6ecfc7b8ec5d0b6ceb9c52c4a85277d6f4
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-11 22:56:59 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T19:56:23.417Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-11T19:56:59.498Z
+Commit: Fix broken links in documentation
