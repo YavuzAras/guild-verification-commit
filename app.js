93c7a84a907624be7a9a577112d3f110e96350e0
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-11 23:00:29 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T19:58:10.814Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-11T20:00:29.156Z
+Commit: Fix broken links in documentation
