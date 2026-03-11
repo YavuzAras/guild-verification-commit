35e63951af7abcb9c53037a26a9f4bbbe61324e3
Poseidon <yavuz.gul.ins@gmail.com>
2026-03-11 22:57:32 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:18:14.564Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-11T19:57:31.620Z
+Commit: Fix memory leak in event handlers
